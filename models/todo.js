513a7ca44a8f571bc89e372f2f8cc20f34a5c92d
marshmello37 <marshmello37@naver.com>
2019-05-18 21:43:27 +0900

    Edit the design and use leftDate function

--- models/todo.js
+++ models/todo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const user = require('./user');
 
 const todoSchema = mongoose.Schema({
-    title: {type : String,required :[true,"Title is required"]},
+    title: {type : String,required :true},
     content: {type : String,required : true},
     date:{type: String,optional: true},
     priority:{type: String,required : true},
--- routes/index.js
+++ routes/index.js
@@ -22,29 +22,37 @@ router.get('/',function(req,res) {
 
 /*W rite the todo list */
 router.post('/write', function(req, res, next){
-  var todo = new Todo({
-    title : req.body.todoTitle.toString(),
-    content : req.body.todoContent.toString(),
-    date : req.body.dates.toString(),
-    priority : req.body.priority});
-  todo.save( (err)=>{
-    if(err)
-      return res.status(500).send("Todo create fail");
-  });
+    const title = req.body.todoTitle.toString();
+    const content = req.body.todoContent.toString();
+    const date = req.body.dates.toString();
+    const priority = req.body.priority.toString();
+    let num;
+    const temp=priority;
+    if(temp=='Important')
+      num=1;
+    else if(temp=='Normal')
+      num=2;
+    else if(temp=='Not Important')
+      num=3;
+    const priority_num=num;
+    const todoInfo = new Todo({title: title,content:content,date:date,priority:priority,priority_num:priority_num});
+    todoInfo.save((err)=>{
+      if(err)
+        return res.status(500).send("Todo create fail");
+      else
+        return res.render('complete.ejs');
+    });
 });
-
 /*Delete the todo list*/
 router.post('/delete/:id',function (req,res) {
   Todo.findByIdAndRemove(req.params.id,function (err,todo) {
     if(err)
       return res.status(500).send("Todo delete fail");
-
   });
 });
 
 /*Edit the todo list*/
 router.post('/edit/:id',function (req,res) {
-  console.log('-2-2-2-2-2-2-2');
   Todo.findByIdAndUpdate(req.params.id,req.body,{new: true},
       function (err, todo) {
     if(err)
