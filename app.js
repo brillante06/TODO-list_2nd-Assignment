15558b4dc9e5b24c4cdcef6f4414134bae9a701e
marshmello37 <marshmello37@naver.com>
2019-05-20 11:56:32 +0900

    Comment on code and gitignore

--- app.js
+++ app.js
@@ -2,28 +2,19 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 const cookieParser = require('cookie-parser');
-var http= require('http');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
 var flash = require("connect-flash");
 var passport = require('passport');
-//const setUpPassPort = require("./setuppasspot");
 mongoose.Promise = global.Promise; // mongoDB 버전 4.11 이상부터 해주어야 에러 안남
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 const mongoDB = 'mongodb://TodoList:k13579@ds145786.mlab.com:45786/heroku_h74qf4ml';
 mongoose.connect(mongoDB,{useNewUrlParser:true});
-//setUpPassPort();
-//app.set("port",process.env.PORT || 3000);
+
 var db = mongoose.connection;
 db.on('error',console.error.bind(console,'MongoDB connection error:'));
-
-
-
-
 
 var app = express();
 
@@ -46,16 +37,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 //   saveUninitialized:true
 // }));
 
-app.use('/', indexRouter);
-//app.use('/user', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -64,5 +52,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+/*connect the heroku with localhost:5000*/
 app.listen(process.env.PORT || 5000);
 module.exports = app;
--- models/todo.js
+++ models/todo.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const user = require('./user');
+//const user = require('./user');
 
 const todoSchema = mongoose.Schema({
     title: {type : String,required :true},
     content: {type : String,required : true},
     date:{type: String,optional: true},
     priority:{type: String,required : true},
-    priority_num:{type: Number}
+    priority_num:{type: Number,required:true}
 });
 
 module.exports= mongoose.model('todo',todoSchema);
